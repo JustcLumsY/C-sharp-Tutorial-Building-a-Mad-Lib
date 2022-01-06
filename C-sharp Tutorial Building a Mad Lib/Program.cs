6b0e23c4bdedd73ca338996105470d68f53744a6
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2022-01-06 08:02:08 +0100

    Add project files.

---
+++ C-sharp Tutorial Building a Mad Lib/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace C_sharp_Tutorial_Building_a_Mad_Lib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue ");
+            Console.WriteLine("I Love " + celebrity);
+            
+        }
+    }
+}
